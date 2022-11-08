f0d6bee87faf48ae28bb772a39acefa3faa46790
JohnJM <drndrndrndrone@gmail.com>
2022-11-08 17:35:49 +0000

    add check for unique username on register

--- controllers/user/user.ts
+++ controllers/user/user.ts
@@ -5,19 +5,31 @@ import { Request, Response } from "express";
 import { getUserData } from "./helper/getUserData";
 
 const register = async (
-    { body: { username, password } = {} }: Request,
+    { body: { username, password, firstName, lastName, email } = {} }: Request,
     res: Response
 ) => {
     try {
+        const userExists = await prisma.user.findFirst({
+            where: {
+                username: {
+                    contains: username,
+                },
+            },
+        });
+        if (userExists) throw new Error("Username already exists");
+
         const user = await prisma.user.create({
             data: {
                 username,
                 password: await hash(password),
+                firstName,
+                lastName,
+                email,
             },
         });
         const [token, maxAge] = createAuthToken(user);
         res.cookie("Authorization", token, { httpOnly: true, maxAge });
-        res.status(200).json({ username: user.username, success: true });
+        res.status(200).json({ success: true });
     } catch (err) {
         const { message } = err as Error;
         res.status(400).json({ error: message });
